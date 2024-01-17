482c6a92712f21d8fd8cf9929a1e0de1ff6234b8
James Bentley <jbentley33@gmail.com>
2024-01-17 11:45:14 -0500

    Update tf variables

--- envs/infra/variables.tf
+++ envs/infra/variables.tf
@@ -1,5 +1,6 @@
 variable "name" {
   type = string
+  default = "james"
 }
 
 variable "vpc_cidr" {
@@ -9,32 +10,39 @@ variable "vpc_cidr" {
 
 variable "domain" {
   type = string
+  default = "plumr.xyz"
 }
 
 variable "blue_subdomain" {
   type = string
+  default = "blue"
 }
 
 variable "green_subdomain" {
   type = string
+  default = "green"
 }
 
 variable "arcgisserver_prefix" {
   type = string
+  default = "arcserver"
 }
 variable "arcgisportal_prefix" {
   type = string
+  default = "arcportal"
 }
 variable "arcgisdatastore_prefix" {
   type = string
+  default = "arcdata"
 }
 variable "region" {
   type = string
+  default = "us-east-1"
 }
 
 data "aws_availability_zones" "az" {
   state         = "available"
-  exclude_names = ["ap-southeast-2a"]
+  exclude_names = ["us-east-1"]
 }
 
 data "aws_elb_service_account" "alb_log" {}
