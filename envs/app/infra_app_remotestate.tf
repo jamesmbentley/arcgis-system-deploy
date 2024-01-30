3d12df9afe1b475a06b2170e0295f9d61e72b480
James Bentley <jbentley33@gmail.com>
2024-01-30 10:13:34 -0500

    Fixed state bucket

--- envs/app/infra_app_remotestate.tf
+++ envs/app/infra_app_remotestate.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "infra" {
   backend = "s3"
 
   config = {
-    bucket = "bentley-tf-remote-stateE"
+    bucket = "bentley-tf-remote-state"
     key    = "dev/infra_v1.0.0.tfstate"
     region = "us-east-1"
   }
